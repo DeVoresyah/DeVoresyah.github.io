6780a398ac3acd411fc3ad97a6b5862936274f56
DeVoresyah ArEst <de.voresyah@gmail.com>
2017-08-22 18:03:31 +0700

    Add gauth.js

---
+++ js/gauth.js
@@ -0,0 +1,78 @@
+// A simple authentication application written in HTML
+// Copyright (C) 2012 Gerard Braad
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Originally based on the JavaScript implementation as provided by Russell Sayers on his Tin Isles blog:
+// http://blog.tinisles.com/2011/10/google-authenticator-one-time-password-algorithm-in-javascript/
+
+function dec2hex(s) {
+    return (s < 15.5 ? '0' : '') + Math.round(s).toString(16);
+}
+
+function hex2dec(s) {
+    return parseInt(s, 16);
+}
+
+function base32tohex(base32) {
+    var base32chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+    var bits = "";
+    var hex = "";
+
+    for (var i = 0; i < base32.length; i++) {
+        var val = base32chars.indexOf(base32.charAt(i).toUpperCase());
+        bits += leftpad(val.toString(2), 5, '0');
+    }
+
+    for (var i = 0; i + 4 <= bits.length; i += 4) {
+        var chunk = bits.substr(i, 4);
+        hex = hex + parseInt(chunk, 2).toString(16);
+    }
+    return hex;
+
+}
+
+function leftpad(str, len, pad) {
+    if (len + 1 >= str.length) {
+        str = Array(len + 1 - str.length).join(pad) + str;
+    }
+    return str;
+}
+
+function checkOtp(account, secretkey) {
+
+    var key = base32tohex(secretkey);
+    var epoch = Math.round(new Date().getTime() / 1000.0);
+    var time = leftpad(dec2hex(Math.floor(epoch / 30)), 16, '0');
+
+    // updated for jsSHA v2.0.0 - http://caligatio.github.io/jsSHA/
+    var shaObj = new jsSHA("SHA-1", "HEX");
+    shaObj.setHMACKey(key, "HEX");
+    shaObj.update(time);
+    var hmac = shaObj.getHMAC("HEX");
+
+    var offset = 0;
+    if (hmac !== 'KEY MUST BE IN BYTE INCREMENTS') {
+       offset = hex2dec(hmac.substring(hmac.length - 1));
+    }
+
+    var qrcode = "https://chart.googleapis.com/chart?chs=200x200&cht=qr&chl=200x200&chld=M|0&cht=qr&chl=otpauth://totp/" + account + "%3Fsecret%3D" + secretkey
+    var otp = (hex2dec(hmac.substr(offset * 2, 8)) & hex2dec('7fffffff')) + '';
+    otp = (otp).substr(otp.length - 6, 6);
+
+    var twofactor = [
+        {"qrcode":qrcode, "otpcode":otp}
+    ];
+    return twofactor;
+}
